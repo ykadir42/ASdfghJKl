f8614c953b1ac24d457eca041dc8438abdd48afd
anish2454 <shenoy.anish.227@gmail.com>
2017-09-16 17:45:42 -0400

    Anish's edits

--- occupations.py
+++ occupations.py
@@ -1,35 +1,44 @@
 import random
 
-def open_file(): #returns the contents of the occupations.csv file
-	source = open( 'occupations.csv', 'rU')
+def open_file(filename): #returns the contents of the occupations.csv file
+	source = open(filename, 'rU')
 	occupations = source.read()
 	source.close()
 	return occupations
 
 
 def make_dict(): #makes a dictionary with occupations as keys and their respective percentages as values
-	occupations = open_file()
-	occupation_list = occupations.split("\n")
+	occupations = open_file("occupations.csv")
+	occupation_list = occupations.split("\n") #makes list of each occupation with its percentage
 	occupations_dict={}
 	for i in occupation_list[1:-2]:
+		#if the occupation has a comma in it
 		if(i[0]=='"'):
+			#splitting by the quote and comma gets us the occupation and the percentage seperated
 			occupation = i[1:].split('",')
 			occupations_dict[occupation[0]] = float(occupation[1])
 		else:
 			occupation = i.split(',')
 			occupations_dict[occupation[0]] = float(occupation[1])
 	return occupations_dict
-occupations_dict = make_dict()
-#print (make_dict())
 
+'''
+1) Keep track of two variables:
+	rand: random float in [0.0, 99.8]
+	sum_percents: sum of all the percents of the occupations visited so far
+2) Go through the dictionary
+3) If rand falls in between [sum_percents, sum_percents + percent of current occupation),
+   return the current occupation.
+This ensures that every occupation has its own range inside [0.0, 99.8). If rand falls within
+an occupation's range, that occupation gets returned.
+'''
 def random_occupation():
-	i=random.random()*99.800000
-	pos=0.0
+	occupations_dict = make_dict()
+	rand=random.random()*99.800000
+	sum_percents=0.0
 	for o in occupations_dict.keys():
-		if (i >= pos and i<pos+occupations_dict[o]):
-			#print i
-			#print str(pos)+"-"+str(pos+occupations_dict[o])
+		if (rand >= sum_percents and rand<sum_percents+occupations_dict[o]):
 			return o
-		pos+=occupations_dict[o]
+		sum_percents+=occupations_dict[o]
 
 print random_occupation()
